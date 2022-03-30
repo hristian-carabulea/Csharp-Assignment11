097021c62a21559dfd5e59fa87f436cf2cd444d6
hristian-carabulea <hristian.carabulea@outlook.com>
2022-03-31 00:33:44 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿class Program
+﻿/// <summary>
+/// Assignment 11: quiz application. It contains loads of MCQs (Multiple Choice Questions).
+/// </summary>
+class Program
 {
     static void Main()
     {
