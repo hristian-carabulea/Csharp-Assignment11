44a5bf39b6acdf2e4a218f0345d5280feeafda04
hristian-carabulea <hristian.carabulea@outlook.com>
2022-03-31 17:05:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@ class Program
 {
     static void Main()
     {
+        //Create an object of Question class and pass no arguments to the constructor
+        Question question1 = new Question();
+        System.Console.WriteLine(question1.correctAnswerLetter);
+
+        //Create an object of Question class and pass value for questionText only to the constructor.
+        Question question2 = new Question("In which continent is Costa Rica situated?");
+        System.Console.WriteLine(question2.correctAnswerLetter);
+
+        //Create an object of Question class and pass values for questionText, optionA, optionB, optionC, optionD and correctAnswerLetter to the constructor.
+        Question question3 = new Question("In which continent is Costa Rica situated?", "Asia", "North America", "South America", "Africa", 'B');
+        System.Console.WriteLine(question3.correctAnswerLetter);
+
+        //Create an object for Question class and pass values for questionText, optionA, optionB, optionC, optionD only to the constructor.
+        Question question4 = new Question() { questionText = "In which continent is Costa Rica situated?", optionA = "Asia", optionB = "North America", optionC = "South America", optionD = "Africa", correctAnswerLetter = 'B' };
+        System.Console.WriteLine(question4.correctAnswerLetter);
+
+        System.Console.Write("Enter any key to end program: ");
+        System.Console.ReadKey();
     }
 }
 
